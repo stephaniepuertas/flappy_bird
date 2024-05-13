a0c0a72f3352de9e7f60bed288b2f8cd277e997c
stephaniepuertas <puertasstephanie@gmail.com>
2024-05-13 01:00:57 -0700

    added reset for game

--- flappy_bird.py
+++ flappy_bird.py
@@ -36,11 +36,20 @@ pass_pipe = False
 # load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
 
-# load scrore
+# load score
 def draw_text(text, font , text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x,y))
+
+# reset numbers function
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x= 100
+    flappy.rect.y= int(screen_height/2)
+    score = 0 
+    return score
 
 # creating bird
 class Bird(pygame.sprite.Sprite):
@@ -116,6 +125,30 @@ class Pipe(pygame.sprite.Sprite):
         if self.rect.right < 0: 
             self.kill ()
 
+# defining button class
+class Button():
+    def __init__(self, x, y , image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y) #providing buttn a position
+
+    def draw(self):
+
+        action = False
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0]== 1:
+                action= True
+
+        # draw button 
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
 
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
@@ -123,6 +156,9 @@ pipe_group = pygame.sprite.Group()
 flappy = Bird(100, int(screen_height/2))
 
 bird_group.add(flappy)
+
+#  create restart button instance
+button= Button(screen_width//2 - 50 , screen_height // 2-100, button_img)
 
 
 run = True
@@ -184,6 +220,12 @@ while run:
             ground_scroll = 0
 
         pipe_group.update()
+
+    #  check for game over and reset
+    if game_over == True:
+        if button.draw() == True:
+            game_over= False
+            score= reset_game()
 
 # event handler
 # iterates through events
